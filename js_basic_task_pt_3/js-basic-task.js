7fba7892eac268c26c77e0a7a722f7b75ab64401
Ivan Zaycev <sinde.fess@gmail.com>
2019-01-09 15:26:34 +0400

    weatherStat() function is done.

--- js_basic_task_pt_3/js-basic-task.js
+++ js_basic_task_pt_3/js-basic-task.js
@@ -56,8 +56,186 @@ console.log(findAnomaly(array, "param"));
 // 13. Weather statistic
 
 function weatherStat(sourceArray, location) {
+    var avgTemp = 0;
+    var counter = 0;
+
+    for (item of sourceArray[location.city][location.date.getFullYear()][location.date.getMonth()]) {
+        avgTemp += item.temp;
+        counter++;
+    }
+
+    avgTemp = avgTemp / counter;
 
     return avgTemp;
 }
 
+array = {
+    "Izhevsk": {
+        "2013": [
+
+        ],
+        "2014": [
+            [
+                {
+                    temp: -12
+                },
+                {
+                    temp: -10
+                },
+                {
+                    temp: -12
+                },
+                {
+                    temp: -5
+                },
+                {
+                    temp: 0
+                },
+                {
+                    temp: 7
+                },
+                {
+                    temp: 10
+                },
+                {
+                    temp: 20
+                },
+                {
+                    temp: 15
+                },
+                {
+                    temp: 7
+                },
+                {
+                    temp: -3
+                },
+                {
+                    temp: -12
+                },
+                {
+                    temp: -12
+                },
+                {
+                    temp: -10
+                },
+                {
+                    temp: -12
+                },
+                {
+                    temp: -5
+                },
+                {
+                    temp: 0
+                },
+                {
+                    temp: 7
+                },
+                {
+                    temp: 10
+                },
+                {
+                    temp: 20
+                },
+                {
+                    temp: 15
+                },
+                {
+                    temp: 7
+                },
+                {
+                    temp: -3
+                },
+                {
+                    temp: -12
+                },
+                {
+                    temp: -12
+                },
+                {
+                    temp: -10
+                },
+                {
+                    temp: -12
+                },
+                {
+                    temp: -5
+                },
+                {
+                    temp: 0
+                },
+                {
+                    temp: 7
+                },
+                {
+                    temp: 10
+                },
+                {
+                    temp: 20
+                },
+                {
+                    temp: 15
+                },
+                {
+                    temp: 7
+                },
+                {
+                    temp: -3
+                },
+                {
+                    temp: -12
+                },
+                {
+                    temp: -12
+                },
+                {
+                    temp: -10
+                },
+                {
+                    temp: -12
+                },
+                {
+                    temp: -5
+                },
+                {
+                    temp: 0
+                },
+                {
+                    temp: 7
+                },
+                {
+                    temp: 10
+                },
+                {
+                    temp: 20
+                },
+                {
+                    temp: 15
+                },
+                {
+                    temp: 7
+                },
+                {
+                    temp: -3
+                },
+                {
+                    temp: -12
+                },
+                {
+                    temp: -12
+                }
+            ],
+            [
+                {
+                    temp: 12
+                }
+            ]
+        ],
+        "2015": [
+
+        ]
+    }
+    
+}
+
 console.log(weatherStat(array, {city: "Izhevsk", date: new Date(2014, 0)}));
+console.log(new Date(2014, 0));
