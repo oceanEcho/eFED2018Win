617c7a4850822e8a4da948a39d0a78d457bd7c57
Ivan Zaycev <sinde.fess@gmail.com>
2019-03-05 18:09:25 +0400

    Added other Renderer childs.

---
+++ js/HistoryRenderer.js
@@ -0,0 +1,9 @@
+function HistoryRenderer() {
+}
+
+HistoryRenderer.prototype = Object.create(Renderer.prototype);
+HistoryRenderer.prototype.constructor = HistoryRenderer;
+
+HistoryRenderer.prototype.renderHistory = function(data) {
+    // some code
+};
